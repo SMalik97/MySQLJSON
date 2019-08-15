c7618b0ea310347d16d2eef1c4a3aff6a8eced6a
Subrata Malik <47157005+SMalik97@users.noreply.github.com>
2019-08-15 16:58:28 +0530

    Add files via upload

---
+++ DatabaseConfig.php
@@ -0,0 +1,13 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "souradeep_db";
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+?>
